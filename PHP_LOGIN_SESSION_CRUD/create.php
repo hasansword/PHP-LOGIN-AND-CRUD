c8a864d5444e143cb6be438caf63b0bce84fab46
Hasan Kılıç <hassan.kllc@outlook.com>
2018-10-29 21:40:26 +0300

    Update create.php

--- PHP_LOGIN_SESSION_CRUD/create.php
+++ PHP_LOGIN_SESSION_CRUD/create.php
@@ -2,8 +2,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <a href="index.php">Listeyi Göster</a><a style="padding-left: 88%" href="quit.php">Çıkış Yap</a>
 
-<form class="col-6" name="yeniKayit" method="post" action="save.php?i=new"><!-- i Get ile gönderilen bir method. bunu başka sayfada yakaladı. -->
-  
+<form class="col-6" name="yeniKayit" method="post" action="save.php?i=new">
 <?php
 
 include_once "control.php";
@@ -38,22 +37,5 @@ include_once "control.php";
     </tr>
   </table>
 </form>
-<!--
-    <div class="form-group col-md-2">
-      <label for="name"></label>
-      <input type="text" class="form-control" id="name" placeholder="Ad" name="name">
-    </div>
-    <div class="form-group col-md-2">
-      <label for="surname"></label>
-      <input type="text" class="form-control" id="surname" placeholder="Soyad" name="surname">
-    </div>
-      <div class="form-group col-md-2">
-        <label for="age"></label>
-        <input type="text" class="form-control" id="age" placeholder="Yaş" name="age">
-      </div>
-<div class="form-group col-md-3">
-    <button type="submit" class="btn btn-primary col-md-6 " style="margin: 4px">Ekle</button>
 
-</div>
--->
 
